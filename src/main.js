74a8e871c20e5523d95a40d59ead8accbb1fb5d0
Miglans <tgmath.h@gmail.com>
2020-04-06 13:28:08 +0100

    taskmanager -1.1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const TASK_COUNT = 3;
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const createSiteMenuTemplate = () => {
   return (
@@ -372,24 +374,23 @@ const createLoadMoreButtonTemplate = () => {
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
+
 };
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const init = () => {
 
-render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
-render(siteMainElement, createBoardTemplate());
+  render(siteHeaderElement, createSiteMenuTemplate());
+  render(siteMainElement, createFilterTemplate());
+  render(siteMainElement, createBoardTemplate());
 
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-const boardElement = siteMainElement.querySelector(`.board`);
+  const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+  const boardElement = siteMainElement.querySelector(`.board`);
+  render(taskListElement, createTaskEditTemplate());
 
-render(taskListElement, createTaskEditTemplate());
+  for (let i = 0; i < TASK_COUNT; i++) {
+    render(taskListElement, createTaskTemplate());
+  }
+  render(boardElement, createLoadMoreButtonTemplate());
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
-}
-
-render(boardElement, createLoadMoreButtonTemplate());
-
-
+};
+init();
